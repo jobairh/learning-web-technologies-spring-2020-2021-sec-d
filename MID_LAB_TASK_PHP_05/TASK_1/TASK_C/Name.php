10ad9d18abf147b4656683c402742e34df7031ca
jobairh1230 <jobairh999@gmail.com>
2021-02-18 21:36:30 +0600

    TASK_1

---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_C/Name.php
@@ -0,0 +1,35 @@
+<?php
+
+	//print_r($_GET['myname']);
+	//print_r($_POST);
+	
+	if(isset($_POST['submit'])){
+
+		$name 		= $_POST['myname'];
+		
+
+		if($name == ""){
+			echo "null submission...";
+		}else{
+			echo $name;
+
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Name</legend>
+			Name: <input type="text" name="myname" value="<?php echo $name; ?>"> 
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_C/Name_2.php
@@ -0,0 +1,32 @@
+<?php
+
+	//print_r($_GET['myname']);
+	//print_r($_POST);
+	
+	/*if(isset($_POST['submit'])){
+		$name 		= $_POST['myname'];
+		
+		if($name == ""){
+			echo "null submission...";
+		}else{
+			echo $name;
+		}
+	}*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Name</legend>
+			Name: <input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname'];} ?>"> 
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
